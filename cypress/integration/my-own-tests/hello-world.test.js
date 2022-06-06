aad2fc1bba47b8865d773ec56cb07d53ba8cb2bf
Yevhen Mykhalov <yevhen_mykhalov@epam.com>
2022-06-06 15:19:54 +0300

    added github action

--- cypress/integration/my-own-tests/hello-world.test.js
+++ cypress/integration/my-own-tests/hello-world.test.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress" />
 
-describe('testing The-internet', ()=>{
-    beforeEach(() => {
-        cy.visit('https://the-internet.herokuapp.com/');
-    });
+// describe('testing The-internet', ()=>{
+//     beforeEach(() => {
+//         cy.visit('https://the-internet.herokuapp.com/');
+//     });
 
-    it('A/B Testing', ()=>{
-        throw new Error('oops');
-    });
-})
+//     it('A/B Testing', ()=>{
+//         throw new Error('oops');
+//     });
+// })
